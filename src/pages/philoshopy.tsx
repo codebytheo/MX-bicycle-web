1ddc3ace9d2101beda25482c6169a9848a517bb3
theoyoth <theobantang1@gmail.com>
2025-08-12 16:06:39 +0900

    try new animation in philosopy page

--- src/pages/philoshopy.tsx
+++ src/pages/philoshopy.tsx
@@ -11,48 +11,48 @@ const Philoshopy = () => {
   const text2 = useRef(null)
   const text3 = useRef(null)
 
-  useGSAP(() => {
-    gsap.set([text1.current,text2.current,text3.current],{
-      filter:"blur(10px)"
-    })
+  // useGSAP(() => {
+  //   gsap.set([text1.current,text2.current,text3.current],{
+  //     opacity:0,
+  //   })
 
-    gsap.to(container.current,{
-      scrollTrigger:{
-        trigger:container.current,
-        pin:true,
-        scrub:1,
-      }
-    })
-    
-    const tl = gsap.timeline()
-    tl.to(text1.current,{
-      filter:"blur(0)"
-    })
-    .to(text2.current,{
-      filter:"blur(0)"
-    })
-    .to(text3.current,{
-      filter:"blur(0)"
-    })
+  //   gsap.timeline({
+  //     scrollTrigger: {
+  //       trigger: container.current,
+  //       start: "top top",
+  //       end: "+=200%",
+  //       pin: true,
+  //       scrub: true,
+  //     }
+  //   })
+  //   .to(text1.current, { opacity:1, })
+  //   .to(text2.current, { opacity:1, })
+  //   .to(text3.current, { opacity:1, })
 
-  },{scope:container})
+
+  // },{scope:container})
+
+  const paragraph1 = '"At MX, we are driven by a simple philosophy: every cyclist deserves a bike that pushes limits. We combine precision engineering, innovative design, and relentless testing to ensure every ride is smoother, faster, and more reliable."'
 
   return (
-    <div ref={container} className="bg-gradient-to-b from-neutral-200 to-blue-50 h-[300vh] px-8 py-10 relative">
-      <div className="h-screen">
-        <div className="grid grid-cols-2 gap-2 h-full">
-          <div className="place-self-end">
-            <p ref={text1} className="text-4xl xl:text-5xl uppercase font-bold text-neutral-900 text-justify"><span className="text-blue-600">"</span>At <span className="text-orange-600">MX</span>, we are driven by a simple philosophy: every cyclist deserves a bike that pushes limits. We combine precision engineering, innovative design, and relentless testing to ensure every ride is smoother, faster, and more reliable.<span className="text-blue-600">"</span>
-            </p>
-          </div>
-          <div className="relative">
-            <p ref={text2} className="text-2xl xl:text-4xl uppercase font-bold text-neutral-900 text-justify"><span className="text-orange-600">"</span>We believe a bicycle is more than just a machine—it’s freedom, health, and adventure. Every bike we offer reflects our passion for cycling and our commitment to helping riders experience the joy of movement, one ride at a time.<span className="text-orange-600">"</span>
-            </p>
-            <p ref={text3} className="text-6xl font-lobster text-orange-600 absolute bottom-20 right-8">"Ride with passion.<br/> Ride with purpose."</p>
-          </div>
+    <div ref={container} className="h-screen w-screen bg-gradient-to-b from-neutral-200 to-blue-50 px-8 py-10 relative">
+      <div className="grid grid-cols-2 gap-2 h-full">
+        <div className="w-full place-self-end text-justify">
+          {
+            paragraph1.match(/MX| |"|[^\s"]+/g)?.map((ch,i) => (
+              <span key={i} className={`text-4xl xl:text-5xl uppercase font-bold ${ch === '"' ? 'text-blue-600' : ch === 'MX' ? 'text-orange-600' : 'text-neutral-900' }`}>
+                {ch === " " ? " " : ch}
+              </span>
+            ) )
+          }
         </div>
-        <p className="absolute left-8 top-10 tracking-widest text-blue-600 font-semibold">. our philoshopy</p>
+        <div className="relative">
+          <p ref={text2} className="text-2xl xl:text-4xl uppercase font-bold text-neutral-900 text-justify"><span className="text-orange-600">"</span>We believe a bicycle is more than just a machine—it’s freedom, health, and adventure. Every bike we offer reflects our passion for cycling and our commitment to helping riders experience the joy of movement, one ride at a time.<span className="text-orange-600">"</span>
+          </p>
+          <p ref={text3} className="text-6xl font-lobster text-orange-600 absolute bottom-20 right-8">"Ride with passion.<br/> Ride with purpose."</p>
+        </div>
       </div>
+      <p className="absolute left-8 top-10 tracking-widest text-blue-600 font-semibold">. our philoshopy</p>
     </div>
   )
 }
