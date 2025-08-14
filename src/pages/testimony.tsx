622d0ed13928eea55d6efaa353c565c85382e7cc
theoyoth <theobantang1@gmail.com>
2025-08-14 14:44:52 +0900

    animate testimony page

--- src/pages/testimony.tsx
+++ src/pages/testimony.tsx
@@ -1,22 +1,66 @@
+import gsap from "gsap"
+import { useGSAP } from "@gsap/react"
+import ScrollTrigger from "gsap/ScrollTrigger"
+import { useRef } from "react"
+
+gsap.registerPlugin(ScrollTrigger)
 
 const Testimony = () => {
+  const container = useRef<HTMLDivElement>(null)
+  const img1 = useRef<HTMLImageElement>(null)
+  const img2 = useRef<HTMLImageElement>(null)
+  const text1 = useRef(null)
+  const text2 = useRef(null)
+
+  useGSAP(() => {
+    gsap.set([img1.current,img2.current], {clipPath:"polygon(0% 0%, 0% 0%, 0% 100%, 0% 100%)"})
+    gsap.set([text1.current,text2.current], {filter:"blur(5px)"})
+
+    const tl = gsap.timeline({
+      scrollTrigger:{
+        trigger: container.current,
+        start: "top bottom",
+        end: "bottom +=90%",
+        scrub:true,
+      }
+    })
+
+    tl.to(img1.current, {
+      clipPath:"polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+      ease:"power2.inOut",
+    })
+    .to(img2.current, {
+      clipPath:"polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+      ease:"power2.inOut"
+    })
+    .to(text1.current,{
+      filter:"blur(0px)",
+      ease:"power2.inOut"
+    })
+    .to(text2.current,{
+      filter:"blur(0px)",
+      ease:"power2.inOut"
+    })
+
+  },{scope:container})
+
   return (
-    <div className="bg-neutral-900 min-h-screen px-8 py-10 relative">
+    <div ref={container} className="bg-neutral-900 min-h-screen px-8 py-10 relative">
       <div className="grid grid-cols-2">
         <div className="w-full h-screen">
-          <img src="/img/2.webp" alt="bicycle" className="saturate-150" />
+          <img ref={img1} src="/img/2.webp" alt="bicycle" className="saturate-150" />
           <div className="mt-8">
-            <p className="text-2xl uppercase text-neutral-200 font-semibold text-justify">"I've bought several bikes over the years, but nothing compares to the one I got from MX. The quality is top-notch, the ride is smooth, and the staff truly know their stuff. Highly recommended!"
+            <p ref={text1} className="text-2xl uppercase text-neutral-200 font-semibold text-justify">"I've bought several bikes over the years, but nothing compares to the one I got from MX. The quality is top-notch, the ride is smooth, and the staff truly know their stuff. Highly recommended!"
+            <span className="block text-xl uppercase text-orange-600 font-bold">james, California</span>
             </p>
-            <p className="text-xl uppercase text-orange-600 font-bold">james, California</p>
           </div>
         </div>
         <div className="w-full place-content-end place-items-end">
-          <img src="/img/8.webp" alt="bicycle" className="w-3/4 saturate-150" />
+          <img ref={img2} src="/img/8.webp" alt="bicycle" className="w-3/4 saturate-150" />
           <div className="w-3/4 flex flex-col mt-8">
-            <p className="text-2xl uppercase text-neutral-200 font-semibold text-justify">"As a competitive cyclist, I need a bike that performs under pressure. The team at helped me choose the perfect model, and it's been a game-changer in my training. Light, responsive, and built to last."
+            <p ref={text2} className="text-2xl uppercase text-neutral-200 font-semibold text-justify">"As a competitive cyclist, I need a bike that performs under pressure. The team at helped me choose the perfect model, and it's been a game-changer in my training. Light, responsive, and built to last."
+            <span className="block text-xl uppercase text-blue-600 font-bold">Klye, Hamburg</span>
             </p>
-            <p className="text-xl uppercase text-blue-600 font-bold">Klye, Hamburg</p>
           </div>
         </div>
       </div>
